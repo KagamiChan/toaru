7c0b8d75e79e25df20889aba0b091412508965cb
かがみ <seki@kagami.me>
2024-04-11 19:50:44 +0800

    feat: add og

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,26 +9,43 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "とある画像の生成コード | Toaru Image Generator",
+  title: "とある画像の生成装置 | Toaru Image Generator",
   description:
     "とあるスタイルの画像生成ツール | Toaru style image generation tool",
-  icons: [{ rel: "icon", url: "/favicon.svg" }, { rel: "icon", url: "/favicon.png" }],
+  icons: [
+    { rel: "icon", url: "/favicon.svg" },
+    { rel: "icon", url: "/favicon.png" },
+  ],
+  openGraph: {
+    title: "とある画像の生成装置 | Toaru Image Generator",
+    description:
+      "とあるスタイルの画像生成ツール | Toaru style image generation tool",
+    images: [
+      {
+        url: "https://toaru.lv0.dev/og.png",
+        width: 1280,
+        height: 600,
+        alt: "とある画像の生成装置 | Toaru Image Generator",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export const generateStaticParams = async () => {
-  return i18nConfig.locales.map((locale) => ({ locale }))
-}
+  return i18nConfig.locales.map((locale) => ({ locale }));
+};
 
 export default function RootLayout({
   children,
-  params: {
-    locale
-  }
+  params: { locale },
 }: {
   children: React.ReactNode;
   params: {
     locale: string;
-  }
+  };
 }) {
   return (
     <html lang={locale}>
